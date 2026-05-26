688c6f3bd093b3c27367ecfe57fc755712c7b26c
Ferretti <compras@mferretti.com.br>
2026-05-26 16:44:07 -0300

    substitui site por versao inline

--- script.js
+++
@@ -1,8 +0,0 @@
-const heroImage = document.querySelector(".hero-media");
-
-if (heroImage) {
-  window.addEventListener("scroll", () => {
-    const offset = Math.min(window.scrollY * 0.018, 10);
-    heroImage.style.transform = `translateY(${offset}px)`;
-  });
-}
