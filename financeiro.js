58c8a1025b6380256024310e75d4c3f4d01dec75
MFerrettiGit <compras@mferretti.com.br>
2026-07-03 17:30:34 -0300

    Atualiza dados das campanhas (automatico 03/07/2026)

--- financeiro.js
+++ financeiro.js
@@ -50,7 +50,7 @@
         ],
         ranking: [],
         regionais: [],
-        gerencia: { totalVendas: 0.00, atingiu: false, premio: 0 }
+        gerencia: { totalVendas: 5234.97, atingiu: false, premio: 0 }
       },
       {
         key: "DKT", nome: "DKT Time de Vendas", modelo: "positivacao", base: "Positivações",
@@ -70,25 +70,25 @@
         ],
         ranking: [
           { setor: "Caraguatatuba", regional: "SUL", vendas: 29, premio: 1500 },
-          { setor: "Americana", regional: "NORDESTE", vendas: 24, premio: 1300 },
           { setor: "S.J. Boa Vista", regional: "NORDESTE", vendas: 29, premio: 1250 },
-          { setor: "Circuito", regional: "NORDESTE", vendas: 21, premio: 1000 },
-          { setor: "Indaiatuba", regional: "SUL", vendas: 13, premio: 450 },
-          { setor: "S.J. dos Campos", regional: "SUL", vendas: 18, premio: 300 },
+          { setor: "Americana", regional: "NORDESTE", vendas: 24, premio: 1250 },
+          { setor: "Circuito", regional: "NORDESTE", vendas: 21, premio: 850 },
+          { setor: "Indaiatuba", regional: "SUL", vendas: 16, premio: 700 },
           { setor: "Rio Claro", regional: "NORDESTE", vendas: 16, premio: 300 },
+          { setor: "S.J. dos Campos", regional: "SUL", vendas: 19, premio: 200 },
+          { setor: "Ribeirão Preto", regional: "NORDESTE", vendas: 18, premio: 200 },
           { setor: "São Carlos", regional: "NORDESTE", vendas: 15, premio: 200 },
-          { setor: "Ribeirão Preto", regional: "NORDESTE", vendas: 16, premio: 150 },
-          { setor: "Sorocaba", regional: "SUL", vendas: 10, premio: 0 },
-          { setor: "Campinas Norte", regional: "SUL", vendas: 10, premio: 0 },
+          { setor: "Sorocaba", regional: "SUL", vendas: 12, premio: 0 },
+          { setor: "Campinas Norte", regional: "SUL", vendas: 12, premio: 0 },
           { setor: "Setor Piracicaba", regional: "NORDESTE", vendas: 8, premio: 0 },
           { setor: "Franca", regional: "NORDESTE", vendas: 8, premio: 0 },
           { setor: "Atibaia", regional: "SUL", vendas: 3, premio: 0 },
-          { setor: "Jundiaí", regional: "SUL", vendas: 1, premio: 0 },
+          { setor: "Jundiaí", regional: "SUL", vendas: 2, premio: 0 },
           { setor: "Itapetininga", regional: "SUL", vendas: 0, premio: 0 }
         ],
         regionais: [
-          { area: "NORDESTE", vendas: 137, premio: 1800 },
-          { area: "SUL", vendas: 84, premio: 0 }
+          { area: "NORDESTE", vendas: 139, premio: 1800 },
+          { area: "SUL", vendas: 93, premio: 0 }
         ],
         gerencia: null
       },
